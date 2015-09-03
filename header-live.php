96e3e84d466d43bcd49e03abfbbbdd2a0768d7c8
Mark Priestap <mapriestap@gmail.com>
2015-09-02 22:26:51 -0400

    add content blocks to english live page

---
+++ header-live.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <main id="main">
+ *
+ * @author Matthias Thom | http://upplex.de
+ * @package upBootWP 1.1
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Magra:400,700|Merriweather:400,700,700italic,400italic,300,300italic' rel='stylesheet' type='text/css'>
+<!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/flipclock.css"> -->
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/jquery.countdown.css"> 
+
+
+<?php wp_head(); ?>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.plugin.min.js"></script> 
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.countdown.min.js"></script>
+<style>
+.page-header {
+	position: relative;
+}
+.schedule {
+	position: absolute;
+	right:0;
+	top: 50%;
+	margin-top: -20px;
+}
+</style>
+</head>
+
+<body id="live">
+	<!-- Go to www.addthis.com/dashboard to customize your tools -->
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54c711a64ebf6b10" async="async"></script>
+
+<div id="page" class="hfeed site">
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header container" role="banner">
+
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<?php $mySnippet = PostSnippets::getSnippet("countdownTimer");
+echo $mySnippet; ?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+				        <div class="navbar-header">
+				            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					            <span class="icon-bar"></span>
+					            <span class="icon-bar"></span>
+					            <span class="icon-bar"></span>
+							</button>
+				            
+				            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"  class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
+				            
+				        </div>
+						
+						<?php 
+						$args = array('theme_location' => 'primary', 
+									  'container_class' => 'navbar-collapse collapse', 
+									  'menu_class' => 'nav navbar-nav',
+									  'fallback_cb' => '',
+			                          'menu_id' => 'live-menu',
+			                          'walker' => new Upbootwp_Walker_Nav_Menu()); 
+						wp_nav_menu($args);
+						?>
+					</div><!-- .col-md-12 -->
+				</div><!-- row -->
+			</div><!-- container -->
+		</nav>
+		
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
--- page-templates/content-live-stream.php
+++ page-templates/content-live-stream.php
@@ -6,22 +6,34 @@
  * @author Bryan VanHaitsma | https://www.reviveourhearts.com
  * @package upBootWP 1.1
  */
-get_header(); ?>
+get_header('live'); ?>
 
 <?php while (have_posts()) : the_post(); ?>
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="content-full-width content-secondary">
 				
 				<?php if(function_exists('upbootwp_breadcrumbs')) upbootwp_breadcrumbs(); ?>
 				<header class="entry-header page-header">
 					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<a class="schedule" href="/schedule">
+						<i class="fa fa-list"></i> 
+						<?php
+
+							if (is_page('envivo')) {
+								echo 'Programa/Horario';
+							}
+							else {
+								echo 'Schedule';
+							}
+
+						?>
+						
+					</a>
 				</header><!-- .entry-header -->
 				
 				<div class="entry-content">
-					
-
-					
+										
 					<?php
 						
 					if ($_COOKIE['live-stream-form-complete']) {
@@ -61,8 +73,91 @@ get_header(); ?>
 						
 					?>
 
+					<div class="row">
+						<div class="col-sm-8">
+							<div class="embed-responsive embed-responsive-16by9">
+								<?php
 
+										if (is_page('live')) {
+											echo '<iframe src="http://new.livestream.com/accounts/10185548/events/3472918/player?width=640&height=360&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no"> </iframe>';
+										}
+										elseif (is_page('live-2')) {
+											echo '<iframe width="640" height="360" src="//www.youtube.com/embed/7nW6Xjd5Djw" frameborder="0" allowfullscreen></iframe>';
+										}
 
+									?>
+							</div>
+							<p class="visible-xs text-center"><a href="/envivo/">Listen en Espa&ntilde;ol &rarr;</a></p>
+						</div>
+						<div class="twitter-stream col-sm-4">
+		<a class="twitter-timeline" href="https://twitter.com/hashtag/truewoman14" data-widget-id="508999278061047808">#truewoman14 Tweets</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+</div>	
+<div class="row">
+	<div class="col-sm-8">
+		<div class="chat">
+			<div style='display:none;'>Live Blog True Woman '14</div><div id='cil-root-f8d0d6b2ff' class='cil-root'><span class='cil-config-data' title='{"altcastCode":"f8d0d6b2ff","server":"www.coveritlive.com","geometry":{"width":"fit","height":650},"configuration":{"newEntryLocation":"bottom","commentLocation":"bottom","replayContentOrder":"chronological","pinsGrowSize":"on","titlePage":"off","skinOverride":"117"}}'>&nbsp;</span></div><script type='text/javascript'>window.cilAsyncInit = function() {cilEmbedManager.init()};(function() {if (window.cilVwRand === undefined) { window.cilVwRand = Math.floor(Math.random()*10000000); }var e = document.createElement('script');e.async = true;var domain = (document.location.protocol == 'http:' || document.location.protocol == 'file:') ? 'http://cdnsl.coveritlive.com' : 'https://cdnslssl.coveritlive.com';e.src = domain + '/vw.js?v=' + window.cilVwRand;e.id = 'cilScript-f8d0d6b2ff';document.getElementById('cil-root-f8d0d6b2ff').appendChild(e);}());</script>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="10000">
+      <div class="carousel-inner">
+				<?php $mySnippet = PostSnippets::getSnippet("liveSidebarAds");
+echo $mySnippet; ?>
+      </div>
+			<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+				<span class="fa fa-chevron-left"></span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+				<span class="fa fa-chevron-right"></span>
+			</a>
+    </div>
+
+	</div>
+</div>
+<hr>
+<h2><span class="section-title" id="schedule"></span> LIVE stream Schedule</h2>
+<h3>Thursday, Oct. 9</h3>
+<table class="table table-schedule table-bordered">
+	<tbody>
+		<tr class="">
+			<td class="column-time">6:45 – 9:45 p.m.</td>
+			<td class="column-session">Evening Session with Joni Eareckson Tada and Nancy Leigh DeMoss</td>
+		</tr>
+	</tbody>
+</table>
+<h3>Friday, Oct. 10</h3>
+<table class="table table-schedule table-bordered">
+	<tbody>
+		<tr class="">
+			<td class="column-time">8:30 – 11:45 a.m.</td>
+			<td class="column-session">Morning Session with Mary Kassian and Janet Parshall</td>
+		</tr>
+		<tr>
+			<td class="column-time">1:30 – 2:30 p.m.</td>
+			<td class="column-session">Breakout Session with Joni Tada (Hymn Sing)</td>
+
+		</tr>
+		<tr class="">
+			<td class="column-time">3:00 – 4:00 p.m.</td>
+			<td class="column-session">Breakout Session with Janet Parshall</td>
+		</tr>
+		<tr class="">
+			<td class="column-time">6:45 – 9:45 p.m.</td>
+			<td class="column-session">Evening Session with Chrissy Toledo and Jim Cymbala</td>
+		</tr>
+	</tbody>
+</table>
+<h3>Saturday, Oct. 11</h3>
+<table class="table table-schedule table-bordered">
+	<tbody>
+		<tr class="">
+			<td class="column-time">8:45 a.m. – 12:00 p.m.</td>
+			<td class="column-session">Morning Session with Nancy Leigh DeMoss</td>
+		</tr>
+	</tbody>
+</table>
 					
 					
 					
